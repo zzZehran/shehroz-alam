03fd6939a6d1ef6c3cc7442878b8fd9c5cda4837
zzZehran <zwhranfarooq24@gmail.com>
2025-06-13 11:36:40 +0530

    added contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,123 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import styles from "./page.module.css";
+
+export default function page() {
+  return (
+    <>
+      <Navbar />
+      <Hero title={"Contact"} navigation={"Contact"} />
+      <div className="container my-5">
+        <h1 className="text-center text-white mb-5">Contact Me</h1>
+        <div className="row">
+          {/* Contact Information */}
+          <div className="col-md-5 mb-4">
+            <div
+              className={`p-4 rounded ${styles.contact_info}`}
+              style={{ backgroundColor: "#151515", color: "#fff" }}
+            >
+              <h3 className="mb-4">Get In Touch</h3>
+              <p>
+                <i className="bi bi-telephone-fill"></i>
+                <strong>Phone:</strong> (+91) 9910772367
+              </p>
+              <p>
+                <i className="bi bi-envelope-at-fill"></i>
+                <strong>Email:</strong> info@shehrozalam.com
+              </p>
+              <p>
+                <i className="bi bi-geo-alt-fill"></i> <strong>Address:</strong>{" "}
+                New Friends Colony, Delhi-IND
+              </p>
+
+              <div className="mt-5">
+                <h5>Follow Me</h5>
+                <div className={`${styles.social_icon} d-flex gap-3`}>
+                  <a
+                    href="https://www.youtube.com/channel/UCfCc7lBhsV4ez2cIjXYJv6A"
+                    className="text-white fs-4"
+                  >
+                    <i className="bi bi-youtube"></i>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/theshehrozalam"
+                    className="text-white fs-4"
+                  >
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                  <a
+                    href="https://x.com/theshehrozalam"
+                    className="text-white fs-4"
+                  >
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/theshehrozalam"
+                    className="text-white fs-4"
+                  >
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Contact Form */}
+          <div className={`col-md-7 ${styles.contact_form}`}>
+            <div className="p-4 rounded">
+              <h3 className="mb-4">Get In Touch</h3>
+              <form>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className="mb-3">
+                      <label htmlFor="name" className="form-label">
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="mb-3">
+                      <label htmlFor="email" className="form-label">
+                        Email address
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        placeholder="name@example.com"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="message" className="form-label">
+                    Message
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    rows="5"
+                    placeholder="Your Message"
+                  ></textarea>
+                </div>
+
+                <button type="submit" className="btn px-4 py-2">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
