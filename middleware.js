e652feff40babc9d5a7fa7131a3d38befed397e3
zzZehran <zwhranfarooq24@gmail.com>
2025-06-16 17:54:31 +0530

    debugging /login page build error

--- middleware.js
+++ middleware.js
@@ -2,10 +2,10 @@ import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth({
   // Protect any routes that start with /studio
-  protectedRoutes: ["/studio"],
+  protectedRoutes: ["/studio/:path"],
   loginPath: "/api/auth/login",
 });
 
-export const config = {
-  matcher: ["/studio/:path*"],
-};
+// export const config = {
+//   matcher: ["/studio/:path*"],
+// };
