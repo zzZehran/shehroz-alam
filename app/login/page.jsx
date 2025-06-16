0566487f294beb20b85e03c928e6649abdd9db49
zzZehran <zwhranfarooq24@gmail.com>
2025-06-16 18:54:25 +0530

    added button to /login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,12 +4,41 @@ import {
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import Link from "next/link";
 
 export default function page() {
+  const buttonStyle = {
+    color: "black",
+    backgroundColor: "white",
+    padding: ".75rem 1.5rem",
+    border: "none",
+    borderRadius: "10px",
+    fontFamily: "Poppins, sans-serif",
+    fontWeight: "700",
+    textDecoration: "none",
+    display: "inline-block",
+    width: "200px", // <-- fixed width for both buttons
+    textAlign: "center", // center text inside the button
+    fontSize: "1.2rem",
+  };
+
   return (
-    <>
-      <LoginLink>Sign in</LoginLink>;<RegisterLink>Sign up</RegisterLink>;
-      <LogoutLink>Logout</LogoutLink>
-    </>
+    <div
+      className="container"
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="d-flex flex-column align-items-center gap-3">
+        <Link href={"/studio"} style={buttonStyle}>
+          Studio
+        </Link>
+        <LoginLink style={buttonStyle}>Sign in</LoginLink>
+        <LogoutLink style={buttonStyle}>Sign out</LogoutLink>
+      </div>
+    </div>
   );
 }
